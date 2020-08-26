225da627e69927d3a90601e5f14e9de1f8ed26dc
John-E <johnennis5@gmail.com>
2020-08-26 21:21:46 +0100

    Added function for a form to add people to contact

--- app.jsx
+++ app.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-
+function AddPersonForm() {
+  const [person, setPerson] = useState("");
+  function handleChange(e) {
+    setPerson(e.target.value);
+  } 
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+  } 
+  
+  return (
+    <form onSubmit={handleSubmit}>
+    <input type="text" placeholder="Add New Contact" onChange={handleChange} value={person} />
+    <button type="submit">Add</button>
+    </form>
+    );
+} 
 
 
 ReactDOM.render(
