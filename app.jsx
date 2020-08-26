e7bbbc1c81d7308041b7bcba28bcd47aeeb67c91
John-E5 <johnennis5@gmail.com>
2020-08-26 21:34:34 +0100

    Added peoplelist function

--- app.jsx
+++ app.jsx
@@ -19,6 +19,14 @@ function AddPersonForm() {
     );
 } 
 
+function PeopleList(props) {
+  const arr = props.data;
+  const listItems = arr.map((val, index)=> <li key ={index}>{val}</li>
+  );
+  return <ul>{listItems}</ul> 
+} 
+
+
 
 ReactDOM.render(
  
